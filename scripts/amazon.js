77a465508b351b3c27d79c85dc45069a565ba96e
yazidtach <yazidtachdev@gmail.com>
2025-06-19 21:44:35 +0100

    Add cart functionality and integrate cart.js
    Introduced a new cart.js file to manage the cart array and included it in amazon.html. Updated amazon.js to handle adding products to the cart, including quantity management and event listeners for 'Add to Cart' buttons.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class = "js-quantity">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -49,7 +49,7 @@
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>
@@ -57,3 +57,39 @@
     }) 
     
     document.querySelector('.products-grid').innerHTML = htmlProduct
+
+    //Loop over every "Add to Cart" button on the page
+
+    document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+      // Add a click event listener to each button
+      button.addEventListener('click', ()=>{
+      // Get the product ID from the data attribute
+        const productId = button.dataset.productId
+        // Variable to store a reference to the matching product in cart if found
+        let matchingItem;
+        
+        cart.forEach((item)=>{
+          // CHECK If the product ID matches one already in the cart and then store it
+          if(productId === item.productId){
+            matchingItem = item}
+        })        
+        // If we found the product in the cart it'll increase the quantity. matchingItem is a truthy value
+        if(matchingItem){
+          matchingItem.quantity += 1 
+        }
+        // if the product didn't exist it will add it to the cart as a new item with quantity 1
+        else {
+          cart.push({
+            productId : productId,
+            quantity : 1
+          })
+        }
+        
+        
+      console.log(cart);
+      
+
+        
+      })
+})
+
