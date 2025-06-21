b377b94db74a59fd7d694eef503664129a063eec
yazidtach <yazidtachdev@gmail.com>
2025-06-21 18:30:18 +0100

    Dynamically render cart items on checkout page
    Replaced hardcoded cart items in checkout.html with dynamic rendering using JavaScript. Updated cart.js to include sample cart data and implemented logic in checkout.js to generate cart item HTML based on cart and product data.

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,10 @@
-export const cart = []
+export const cart = [ {
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity : 1
+},{
+    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity : 1
+}, {
+    productId : "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    quantity : 5
+}]
